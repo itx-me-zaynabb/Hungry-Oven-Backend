a5d89951c0fbd5b8b54125641baeec15ea0700d5
itx-me-zaynabb <zainabfattimaa@gmai.com>
2026-05-18 18:23:04 +0500

    do some changing

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable prettier/prettier */
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: [
-      "http://localhost:3000",
       "http://localhost:5173",
-      "https://hungry-oven.vercel.app"
+      "https://the-hungry-oven.vercel.app",
     ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     credentials: true,
   });
 
-  await app.listen(process.env.PORT || 3000, "0.0.0.0");
+  await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
