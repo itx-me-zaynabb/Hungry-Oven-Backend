1a26c588e5fc528817f410bc6aae4a10de6d88d0
itx-me-zaynabb <zainabfattimaa@gmai.com>
2026-05-18 15:32:25 +0500

    add vercel.json ffile

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "https://hungry-oven.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT || 3000, "0.0.0.0");
 }
 bootstrap();
